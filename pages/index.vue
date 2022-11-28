6fb957e65b897468bc4a19f5068c2eea6d11914d
Hans <hans.villarroel@usach.cl>
2022-11-27 22:59:45 -0300

    Added static login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,71 @@
+
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-app light class="login">
+
+    <v-container fluid fill-height class="blurBG">
+      <v-layout align-center>
+        <v-flex md6 offset-md3>
+          <v-card elevation="4" class="blurCard" tag="section" >
+            <v-card-title>
+              <v-layout align-center justify-space-between>
+                <h1 class="headline">
+                  SED USACH
+                </h1>
+                <v-img lazy-src="/logoUsach.png" max-height="150" max-width="250" src="/logoUsach.png"></v-img>
+              </v-layout>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <p> Porfavor ingrese sus datos de acceso</p>
+              <v-form>
+                <v-text-field v-model="username" outline label="Username" type="text"></v-text-field>
+                <v-text-field v-model="password" outline hide-details label="Password" type="password"></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-divider></v-divider>
+            <!-- Editar estas acciones una vez backend sea implementado -->
+            <v-card-actions :class="{ 'pa-3': $vuetify.breakpoint.smAndUp }">
+              <v-btn color="info">
+                ¿Olvidaste tu contraseña?
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="info" :large="$vuetify.breakpoint.smAndUp">
+                <v-icon left> :=:</v-icon>
+                Ingresar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+</div>
+  </v-app>
 </template>
+
+<style scoped>
+.login {
+  background-image: url('/diinf.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.blurBG{
+
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+
+}
+
+.blurCard {
+  opacity: 95%;
+
+}
+</style>
+
 
 <script>
 export default {
   name: 'IndexPage',
+  layout: 'empty',
 }
 </script>
